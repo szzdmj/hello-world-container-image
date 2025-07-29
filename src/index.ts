49d28d52bae0a9d539ef0aab89a565b39e17a81c
szzdmj <chuanfuyin001@gmail.com>
2025-07-30 06:30:41 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-// src/index.ts
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return env.container.fetch(request);
-  },
-};
+  async fetch(request: Request, env: any, ctx: ExecutionContext): Promise<Response> {
+    const url = new URL(request.url);
+    const pathname = url.pathname;
 
-export interface Env {
-  container: Fetcher;
+    // 显示容器地址用于调试
+    if (pathname === "/__probe") {
+      return new Response("Container is alive.", { status: 200 });
+    }
+
+    // 所有请求转发到容器
+    return env.container.fetch(request);
+  }
 }
